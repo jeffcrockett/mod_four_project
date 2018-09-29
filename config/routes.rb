d8d9e8e03dc34df5e04116d5afc8e87bf4c2a159
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-09-28 22:08:15 -0400

    Added auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
-      resources :picks, :users
+      resources :picks
+      resources :users, only: %i[create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_203844) do
+ActiveRecord::Schema.define(version: 2018_09_29_001715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_09_28_203844) do
     t.string "img_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
